4608a8c9f66593decf679c3cb5c6b799107aa059
Casper da Costa-Luis <casper.dcl@physics.org>
2025-05-13 10:39:43 +0100

    replace pybind11 -> nanobind

--- numcu/src/numcu.cu
+++ numcu/src/numcu.cu
@@ -4,35 +4,31 @@
  * Copyright (2022) Casper da Costa-Luis
  */
 #include "elemwise.h"          // div, mul, add
-#include <pybind11/pybind11.h> // pybind11
+#include <nanobind/nanobind.h> // nanobind, NB_MODULE
+#include <nanobind/ndarray.h>  // ndarray
 #include <pycuvec.cuh>         // CUDA_PyErr
 
-namespace py = pybind11;
+namespace nb = nanobind;
+template <typename T> using Arr = const nb::ndarray<T>;
 
 template <typename T>
-void elem_div(py::buffer num, py::buffer den, py::buffer dst, T zeroDivDefault) {
-  py::buffer_info src_num = num.request(), src_den = den.request(), dst_out = dst.request(true);
-  div(static_cast<T *>(dst_out.ptr), static_cast<T *>(src_num.ptr), static_cast<T *>(src_den.ptr),
-      dst_out.size, zeroDivDefault);
+void elem_div(Arr<const T> &num, Arr<const T> &den, Arr<T> &dst, T zeroDivDefault) {
+  div(dst.data(), num.data(), den.data(), dst.size(), zeroDivDefault);
   if (CUDA_PyErr()) throw std::runtime_error("CUDA kernel");
 }
 
-template <typename T> void elem_mul(py::buffer a, py::buffer b, py::buffer dst) {
-  py::buffer_info src_a = a.request(), src_b = b.request(), dst_out = dst.request(true);
-  mul(static_cast<T *>(dst_out.ptr), static_cast<T *>(src_a.ptr), static_cast<T *>(src_b.ptr),
-      dst_out.size);
+template <typename T> void elem_mul(Arr<const T> &a, Arr<const T> &b, Arr<T> &dst) {
+  mul(dst.data(), a.data(), b.data(), dst.size());
   if (CUDA_PyErr()) throw std::runtime_error("CUDA kernel");
 }
 
-template <typename T> void elem_add(py::buffer a, py::buffer b, py::buffer dst) {
-  py::buffer_info src_a = a.request(), src_b = b.request(), dst_out = dst.request(true);
-  add(static_cast<T *>(dst_out.ptr), static_cast<T *>(src_a.ptr), static_cast<T *>(src_b.ptr),
-      dst_out.size);
+template <typename T> void elem_add(Arr<const T> &a, Arr<const T> &b, Arr<T> &dst) {
+  add(dst.data(), a.data(), b.data(), dst.size());
   if (CUDA_PyErr()) throw std::runtime_error("CUDA kernel");
 }
 
-using namespace pybind11::literals;
-PYBIND11_MODULE(numcu, m) {
+using namespace nb::literals;
+NB_MODULE(numcu, m) {
   m.doc() = "NumCu external module.";
   m.def("div", &elem_div<float>, "Elementwise division.", "numerator"_a, "divisor"_a, "output"_a,
         "default"_a = FLOAT_MAX);
